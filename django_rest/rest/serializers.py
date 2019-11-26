82183998d996944cb36c52d3c82496e15ece5950
Oscar Diaz <oscar.diaz@thebeat.co>
2019-11-26 18:43:46 -0500

    [FEAT] update model serializer to hyperlink

--- django_rest/rest/serializers.py
+++ django_rest/rest/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import Task
 
 
-class TaskSerializer(serializers.ModelSerializer):
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'due_date')
+        fields = ('id', 'url', 'title', 'description', 'due_date')
