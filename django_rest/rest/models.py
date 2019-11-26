5194ca3c9fbb83c20276f37f803ea2f8e1da2a87
Oscar Diaz <oscargdi22@gmail.com>
2019-11-25 23:32:33 -0500

    [FEAT] add first model

--- django_rest/rest/models.py
+++ django_rest/rest/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, null=True)
+    description = models.TextField(null=True)
+    due_date = models.DateTimeField(null=True)
