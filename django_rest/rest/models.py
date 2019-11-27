58972738e27c9534fed3bde89020927c08cbf6c5
Oscar Diaz <oscar.diaz@thebeat.co>
2019-11-26 19:10:35 -0500

    [FEAT] add str to models

--- django_rest/rest/models.py
+++ django_rest/rest/models.py
@@ -10,11 +10,17 @@ class Project(models.Model):
     title = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Tag(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, null=True)
     color = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Task(models.Model):
@@ -24,3 +30,6 @@ class Task(models.Model):
     due_date = models.DateTimeField(default=datetime.now)
     project = models.ForeignKey(Project, null=True, on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
